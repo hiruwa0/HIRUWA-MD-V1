0f2f5ed921f53ffbb0484a75569f1b711302392b
KING-RAVI-LITE <raviproject418@gmail.com>
2025-04-19 12:32:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "",
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+    CAPTION: process.env.CAPTION || "*🧑‍💻⃝𝙃𝙞𝙍𝙐𝙒𝘼 𝙈𝘿 𝙏𝙀𝘾𝙃➝💥*",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
+    MODE: process.env.MODE || "private",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || '🧑‍💻⃝𝙃𝙞𝙍𝙐𝙒𝘼 𝙈𝘿 𝙏𝙀𝘾𝙃➝💥',
+    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || '94740482244',
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/x04f5n.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M AWAIS-MD WHATSAPP BOT 😊♻️",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "🧑‍💻⃝𝙃𝙞𝙍𝙐𝙒𝘼 𝙈𝘿➝💥",
+    ANTI_VV: process.env.ANTI_VV || "true",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'same' if you want to resend deleted message in same chat
+    DEV: process.env.DEV || "94704349801", //replace with your whatsapp number
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`*𝗔𝗨𝗧𝗢 𝗦𝗧𝗔𝗧𝗨𝗦 𝗦𝗘𝗘𝗡 𝗕𝗬 𝗕𝗢𝗧 𝗛𝗶𝗥𝗨𝗪𝗔-𝗠𝗗🪄`*`",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+    };
+    
